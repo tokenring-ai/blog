95b69b8c68d1f8c3b3b163afffed82eb5f4b0448
Mark Dierolf <mdierolf@cloudquote.io>
2025-10-04 11:09:12 -0700

    Refactored package to implement automatic installation & configuration in TokenRing apps

--- index.ts
+++ index.ts
@@ -1,14 +1,27 @@
-import {TokenRingPackage} from "@tokenring-ai/agent";
+import {AgentTeam, TokenRingPackage} from "@tokenring-ai/agent";
+import {z} from "zod";
+import BlogService from "./BlogService.ts";
+import * as chatCommands from "./chatCommands.ts";
 import packageJSON from './package.json' with {type: 'json'};
+import * as tools from "./tools.ts";
+
+export const BlogConfigSchema = z.object({
+  providers: z.record(z.string(), z.any())
+}).optional();
 
 export const packageInfo: TokenRingPackage = {
   name: packageJSON.name,
   version: packageJSON.version,
   description: packageJSON.description,
-  tools,
-  chatCommands,
+  install(agentTeam: AgentTeam) {
+    const config = agentTeam.getConfigSlice('blog', BlogConfigSchema);
+    if (config) {
+      const service = new BlogService();
+      agentTeam.services.register(service);
+    }
+    agentTeam.addTools(packageInfo, tools);
+    agentTeam.addChatCommands(chatCommands);
+  },
 };
 
 export {default as BlogService} from "./BlogService.ts";
-import * as tools from "./tools.ts";
-import * as chatCommands from "./chatCommands.ts";
